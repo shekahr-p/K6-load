76ebf543a3b4c8d716b8658050da31ec1ff03937
Shekhar <shekhardeshmukh0490@gmail.com>
2022-11-25 10:25:48 +0530

    env

--- my-script-file.js
+++ my-script-file.js
@@ -5,16 +5,18 @@
 import http from 'k6/http'
 
 // main function where user will spread
-export const options = {
-    vus: 10,
-    duration: '5m',
-  }
+// export const options = {
+//     vus: 10,
+//     duration: '5m',
+//   }
   export const options1 = {
-
+//stages array
     stages: [
       { duration: '10s', target: 5 },
       { duration: '20s', target: 3 }
     ]
+    , vus: 10,
+    duration: '1m',
   }
 export default function () {
 
